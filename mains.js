bf3cb7d20ed76d74409204b1602cd0ef748c093a
Vinicius Ribeiro <vinicius.basto.r@gmail.com>
2023-04-29 16:06:55 -0300

    alteração exercicio_html_js

---
+++ mains.js
@@ -0,0 +1,11 @@
+const form = document.getElementById('form_valores');
+
+function confirmaValores(primeiro_valor, segundo_valor){
+    const confirmação = 
+}
+
+form.addEventListener('submit', function (e){
+    e.preventDefault();
+})
+
+console.log(form);

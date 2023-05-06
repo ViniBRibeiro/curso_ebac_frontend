82ce268eee04a90fc1a72312b4bb41ef72a3a0df
Vinicius Ribeiro <vinicius.basto.r@gmail.com>
2023-05-05 21:13:24 -0300

    Alteração exercicio_html_js

--- mains.js
+++ mains.js
@@ -1,17 +1,19 @@
 const form = document.getElementById('form_valores');
 
+form.addEventListener('submit', function (e){
+    e.preventDefault();
+})
+
 function confirmaValores(primeiroValor, segundoValor){
     const primeiroValor = document.getElementById('primeiroValor')
     const segundoValor = document.getElementById('segundoValor')
 
     if(segundoValor > primeiroValor){
-        return true;
+        
         
     }else{
-        return false;
+        
     }
 }
 
-form.addEventListener('submit', function (e){
-    e.preventDefault();
-})
+
